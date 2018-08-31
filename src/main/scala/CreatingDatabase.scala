ae776db97e40d0a46f5facbb8ca31639029dd1c5
RoxanaTesileanu <roxana.te@web.de>
2018-08-31 15:18:06 -0400

    prerequisites on using mongodb in scala

--- src/main/scala/CreatingDatabase.scala
+++ src/main/scala/CreatingDatabase.scala
@@ -4,7 +4,7 @@ The functions presented here will help you do CRUD processing. If you need addit
 PREREQUISITES:
 - you should install mongodb on your machine (go to the official page and follow the steps for your distribution)
 - you have to allow the port 27017 used by the localhost for mongo ($ sudo ufw allow 27017)
-- you have to start the mongod server in a terminal ($ sudo service mogod start). At the end of the session you should close the server ($sudo service mongod stop). 
+- you have to start the mongod service in a terminal ($ sudo service mogod start). At the end of the session you should close the service ($sudo service mongod stop). 
 - in another terminal, navigate to the project's directory and start sbt and the scala console and test the codes within the bellow object (Copy-paste it and wait to see the how the REPL evaluates it. This will help you undestand how mongodb behaves.)
 
 */
