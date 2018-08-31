40bcfa9434b950642c5eb8a40043a2c2cc2260b1
RoxanaTesileanu <roxana.te@web.de>
2018-08-31 14:55:43 -0400

    creating a database in mongodb using the scala driver

---
+++ src/main/scala/CreatingDatabase.scala
@@ -0,0 +1,11 @@
+/* This object helps you create a MongoDB database called "MyStuff". Within this database you will create a collection called "FruitsAndVegetables".
+The functions presented here will help you do CRUD processing. If you need additional guidance check the official reference of the mongodb-scala-driver.
+*/
+
+
+object CreatingDatabase{
+
+
+
+
+}
